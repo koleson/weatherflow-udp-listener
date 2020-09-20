2df7457025127838a4c15ddfe19c9cc43ec39359
Kiel Oleson <kiel@jusquici.org>
2020-09-20 00:42:51 -0700

    slightly better constructor?

--- listen.py
+++ listen.py
@@ -543,7 +543,8 @@ def influxdb_publish(event, data):
             print ("publishing %s to influxdb [%s:%s]: %s" % (event,args.influxdb_host, args.influxdb_port, payload))
 
         # kmo 2020-09-20 00h26:  this is unlikely to be the right API for Point
-        point = Point(payload)
+        point = Point(dictionary: payload)
+            .time(data['timestamp'])
         
         write_api = client.write_api()
         
